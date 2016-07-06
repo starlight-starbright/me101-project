634ecd6fff2ee8c08f109197dbcc86eeaf9063ec
starlight-starbright <alpha.omega501@gmail.com>
2016-07-05 23:06:47 -0400

    Added e-stop, minor revisions

--- Main.c
+++ Main.c
@@ -1,54 +1,81 @@
-void rotate(int degrees);
+task arm_emergency_stop();
+void rotate(int motor_number, int degrees);
+void store_color(int color);
+int get_color();
 
 task main()
 {
+	startTask(arm_emergency_stop);
+
 	int color = 0;
 	SensorType[S1] = sensorColorNxtFULL;
 
 	int colorAccept[6] = {1, 0, 0, 0, 1, 0};
-	/*
-	1 - Black/Purple
-	2 - Blue
-	3 - Green
-	4 - Yellow
-	5 - Red
-	6 - White
-	*/
-
-
+	//1 - Black/Purple
+	//2 - Blue
+	//3 - Green
+	//4 - Yellow
+	//5 - Red
+	//6 - White
 
 	while(true)
 	{
 		//Rotate to color sensor
-		rotate(90);
+		rotate(0, 90);
 		wait1Msec(250);
 
 		//Detect color
-		color = SensorValue[S1];
+		color = 5; //get_color();
 		displayString(0, "%d", color);
 		wait1Msec(1000);
 		eraseDisplay();
 
 		if(colorAccept[color - 1] == 1)
 		{
-			rotate(90);
+			store_color(color);
 			wait1Msec(250);
+			rotate(0, 90);
+			wait1Msec(250);
+
 		}
 		else //color rejected
 		{
-			rotate (-180);
+			rotate (0, -180);
 			wait1Msec(250);
-			rotate (90);
+			rotate (0, 90);
 			wait1Msec(250);
 		}
+		EndTimeSlice();
 	}
 }
 
-void rotate(int degrees)
+task arm_emergency_stop()
 {
-	//Rotate to color sensor
-	nMotorEncoder[0] = 0;
-	motor[0] = (degrees / abs(degrees)) * 5;
-	while(abs(nMotorEncoder[0]) < abs(degrees)){}
-	motor[0] = 0;
+	while(true)
+	{
+		if(SensorValue[S2] == 1)
+		{
+			stopAllTasks();
+		}
+		EndTimeSlice();  // this prevents the current task from hogging the CPU
+	}
+}
+
+void rotate(int motor_number, int degrees)
+{
+	nMotorEncoder[motor_number] = 0;
+	motor[motor_number] = (degrees / abs(degrees)) * 5;
+	while(abs(nMotorEncoder[motor_number]) < abs(degrees)){}
+	motor[motor_number] = 0;
+}
+
+void store_color(int color)
+{
+	rotate(1, color * 60);
+}
+
+int get_color() //update later for full RGB if possible
+{
+	int color = SensorValue[S1];
+	return color;
 }
